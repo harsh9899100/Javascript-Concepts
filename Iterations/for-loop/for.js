d684448625b93c6c4bbd042d77b7e3e0082769db
Harsh Patel <123819080+harsh9899100@users.noreply.github.com>
2025-01-09 09:52:59 +0000

    updated...

--- Iterations/for-loop/for.js
+++ Iterations/for-loop/for.js
@@ -13,11 +13,11 @@ for (let i = 0; i <= 10; i++) {
 
 // ------------------------------------------------------------
 
-for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-    for (let j = 1; j <= 10; j++) {
-      //console.log(`Inner loop value ${j} and inner loop ${i}`);
-      //console.log(i + '*' + j + ' = ' + i*j );
+for (let a = 1; b <= 10; a++) {
+    console.log(`Outer loop value: ${a}`);
+    for (let b = 1; b <= 10; b++) {
+      console.log(`Inner loop value ${b} and inner loop ${a}`);
+      console.log(a + '*' + b + ' = ' + a*b );
     }
   }
 
